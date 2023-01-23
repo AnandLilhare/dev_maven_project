425dda10fa23f32ecea8035a49a1757f7fc50865
AnandLilhare <anandlilhare69@gmail.com>
2023-01-23 10:52:26 +0000

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World. This is my maven project and I am excuting this on jenkin job for assigement" );
+    }
+}
